860540b763951460fe6a301b34e07944e5815799
OscarV2 <ing.odvega@gmail.com>
2016-01-09 14:19:57 -0500

    Primer commit

---
+++ app/src/main/java/com/example/oscar/myambu/primera_pantalla.java
@@ -0,0 +1,13 @@
+package com.example.oscar.myambu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class primera_pantalla extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primera_pantalla);
+    }
+}
